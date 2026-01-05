63946482810d961c7d5f2cc758bd50cfed3f7266
unknown <aldrincantiga30@gmail.com>
2026-01-06 01:03:36 +0800

    Update API URL for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // When we deploy, we will change this URL to the real backend URL
-const API_URL = 'http://localhost:5000/api'; 
+// Change this line!
+const API_URL = 'https://mindcare-server-v78p.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
